5adeff29dd5b7d855ac6552d32879bce141c0797
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2024-06-22 22:15:58 -0600

    Correccion de codigo para las vocales

--- VOCALES Y VALIDACIONES.cpp
+++ VOCALES Y VALIDACIONES.cpp
@@ -1,35 +1,39 @@
-// 1CV11 HERNANDEZ GONZALEZ JOSEPH FABRIZZIO VOCALES Y VALIDACIONES 
-#include <iostream>
-#include <map>
+#include <iostream> //1CV11 HERNANDEZ GONZALEZ JOSEPH FABRIZZIO
+#include <string>
+
 using namespace std;
 
-void countVowels(char str[]) {
-    map<char, int> vowelCount = {
-        {'a', 0}, {'e', 0}, {'i', 0}, {'o', 0}, {'u', 0},
-        {'á', 0}, {'é', 0}, {'í', 0}, {'ó', 0}, {'ú', 0},
-        {'A', 0}, {'E', 0}, {'I', 0}, {'O', 0}, {'U', 0},
-        {'Á', 0}, {'É', 0}, {'Í', 0}, {'Ó', 0}, {'Ú', 0}
-    };
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (vowelCount.find(str[i]) != vowelCount.end()) {
-            vowelCount[str[i]]++;
-        }
-    }
-
-    cout << "Cuenta de vocales en la frase:" << endl;
-    for (auto &pair : vowelCount) {
-        if (pair.second > 0) {
-            cout << pair.first << ": " << pair.second << endl;
-        }
-    }
-}
-
 int main() {
-    char myString[200];
-    cout << "Ingrese una frase: ";
-    cin.getline(myString, 200);
-    countVowels(myString);
+    string input;
+    int vowels[5] = {0}; // a, e, i, o, u
+
+    cout << "Ingresa una cadena: ";
+    getline(cin, input);
+
+    // Transformar a minÃºsculas
+    for (size_t i = 0; i < input.length(); ++i) {
+        if (input[i] >= 'A' && input[i] <= 'Z') {
+            input[i] = input[i] + ('a' - 'A');
+        }
+    }
+
+    // Contar las vocales
+    for (size_t i = 0; i < input.length(); ++i) {
+        switch (input[i]) {
+            case 'a': vowels[0]++; break;
+            case 'e': vowels[1]++; break;
+            case 'i': vowels[2]++; break;
+            case 'o': vowels[3]++; break;
+            case 'u': vowels[4]++; break;
+        }
+    }
+
+    cout << "Conteo de vocales: " << endl;
+    cout << "a: " << vowels[0] << endl;
+    cout << "e: " << vowels[1] << endl;
+    cout << "i: " << vowels[2] << endl;
+    cout << "o: " << vowels[3] << endl;
+    cout << "u: " << vowels[4] << endl;
+
     return 0;
 }
-
